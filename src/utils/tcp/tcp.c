030a7b087fabc99447efb8b7ecaee412a1c58c20
gcs2508 <gcs2508@163.com>
2016-07-26 12:45:25 +0800

    TINY trans files BUG

--- src/utils/tcp/tcp.c
+++ src/utils/tcp/tcp.c
@@ -60,9 +60,9 @@ int tcp_recv_message(int fd,void *buf,int size) {
 
 int tcp_uninit() {
 
-	tnet_socket_close(tcp_socket);
+//	tnet_socket_close(tcp_socket);
 	TSK_OBJECT_SAFE_FREE(tcp_socket);
-	tcp_socket = NULL;
+//	tcp_socket = NULL;
 
 	return 0;
 }
